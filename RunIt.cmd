feaa24320e213fd49b0ae36423b78c27a9737507
Lain MultipleInstances <zlainsama@163.com>
2019-12-07 15:51:54 +0800

    Update RunIt.cmd

--- RunIt.cmd
+++ RunIt.cmd
@@ -6,9 +6,9 @@ del /Q /F /S ..\unbound\unbound_ad_servers
 del /Q /F /S ..\unbound\unbound_blocked_servers
 del /Q /F /S ..\unbound\unbound_localforwards
 
-cp AdServers_Processed ..\unbound\unbound_ad_servers
-cp MalwareDomains_Processed ..\unbound\unbound_blocked_servers
-cp LocalForwards_Processed ..\unbound\unbound_localforwards
+copy AdServers_Processed ..\unbound\unbound_ad_servers
+copy MalwareDomains_Processed ..\unbound\unbound_blocked_servers
+copy LocalForwards_Processed ..\unbound\unbound_localforwards
 
 ..\unbound\unbound-checkconf.exe ..\unbound\service.conf
 
