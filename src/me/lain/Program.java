0e4880a74e805d01ca44aa0c2c6eb4eb3b752620
Lain MultipleInstances <zlainsama@163.com>
2020-12-19 22:26:40 +0800

    update

--- src/me/lain/Program.java
+++ src/me/lain/Program.java
@@ -18,54 +18,39 @@ import java.util.Optional;
 import java.util.concurrent.ForkJoinTask;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class Program
-{
+public class Program {
 
-    interface Processor
-    {
-
-        void accept(BufferedReader in, BufferedWriter out) throws IOException;
-
-    }
-
+    static final int TotalTasks = 2;
     static final int MaxRetries = 5;
     static final String AdServers = "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=nohtml&showintro=1&startdate%5Bday%5D=&startdate%5Bmonth%5D=&startdate%5Byear%5D=";
-    static final String MalwareDomains = "https://mirror1.malwaredomains.com/files/BOOT";
     static final String LocalForwards = "https://github.com/felixonmars/dnsmasq-china-list/raw/master/accelerated-domains.china.conf";
     static final AtomicInteger CompletedTasks = new AtomicInteger();
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         List<ForkJoinTask<?>> tasks = new ArrayList<>();
         tasks.add(ForkJoinTask.adapt(Program::processAdServers));
-        tasks.add(ForkJoinTask.adapt(Program::processMalwareDomains));
         tasks.add(ForkJoinTask.adapt(Program::processLocalForwards));
         ForkJoinTask.invokeAll(tasks);
-        if (CompletedTasks.get() != 3)
+        if (CompletedTasks.get() != TotalTasks)
             System.exit(1);
     }
 
-    static void process(Path in, Path out, Processor processor) throws IOException
-    {
+    static void process(Path in, Path out, Processor processor) throws IOException {
         System.out.println(String.format("> Processing %s", in));
-        try (BufferedReader rIn = Files.newBufferedReader(in); BufferedWriter wOut = Files.newBufferedWriter(out))
-        {
+        try (BufferedReader rIn = Files.newBufferedReader(in); BufferedWriter wOut = Files.newBufferedWriter(out)) {
             processor.accept(rIn, wOut);
         }
         System.out.println(String.format("> Completed %s", out));
     }
 
-    static void processAdServers()
-    {
+    static void processAdServers() {
         resource(AdServers)
                 .ifPresent(remote -> read(remote, Paths.get("AdServers"), MaxRetries)
                         .ifPresent(local -> {
-                            try
-                            {
+                            try {
                                 process(local, Paths.get("AdServers_Processed"), (r, w) -> {
                                     String l = null;
-                                    while ((l = r.readLine()) != null)
-                                    {
+                                    while ((l = r.readLine()) != null) {
                                         if (l.isEmpty())
                                             continue;
                                         w.write("local-zone: ");
@@ -79,25 +64,20 @@ public class Program
                                     }
                                 });
                                 CompletedTasks.getAndIncrement();
-                            }
-                            catch (IOException e)
-                            {
+                            } catch (IOException e) {
                                 e.printStackTrace();
                             }
                         }));
     }
 
-    static void processLocalForwards()
-    {
+    static void processLocalForwards() {
         resource(LocalForwards)
                 .ifPresent(remote -> read(remote, Paths.get("LocalForwards"), MaxRetries)
                         .ifPresent(local -> {
-                            try
-                            {
+                            try {
                                 process(local, Paths.get("LocalForwards_Processed"), (r, w) -> {
                                     String l = null;
-                                    while ((l = r.readLine()) != null)
-                                    {
+                                    while ((l = r.readLine()) != null) {
                                         if (l.isEmpty() || l.startsWith("#"))
                                             continue;
                                         String[] al = l.split("/", 3);
@@ -112,55 +92,17 @@ public class Program
                                     }
                                 });
                                 CompletedTasks.getAndIncrement();
-                            }
-                            catch (IOException e)
-                            {
+                            } catch (IOException e) {
                                 e.printStackTrace();
                             }
                         }));
     }
 
-    static void processMalwareDomains()
-    {
-        resource(MalwareDomains)
-                .ifPresent(remote -> read(remote, Paths.get("MalwareDomains"), MaxRetries)
-                        .ifPresent(local -> {
-                            try
-                            {
-                                process(local, Paths.get("MalwareDomains_Processed"), (r, w) -> {
-                                    String l = null;
-                                    while ((l = r.readLine()) != null)
-                                    {
-                                        if (l.isEmpty() || l.startsWith("/"))
-                                            continue;
-                                        String[] al = l.split(" ", 3);
-                                        w.write("local-zone: ");
-                                        w.write(al[1]);
-                                        w.write(" redirect");
-                                        w.newLine();
-                                        w.write("local-data: \"");
-                                        w.write(al[1]);
-                                        w.write(" A 0.0.0.0\"");
-                                        w.newLine();
-                                    }
-                                });
-                                CompletedTasks.getAndIncrement();
-                            }
-                            catch (IOException e)
-                            {
-                                e.printStackTrace();
-                            }
-                        }));
-    }
-
-    static Optional<Path> read(URL resource, Path local, int maxTries)
-    {
+    static Optional<Path> read(URL resource, Path local, int maxTries) {
         int tries = 0;
-        while (tries++ < maxTries)
-        {
+        while (tries++ < maxTries) {
             System.out.println(String.format("> Downloading %s (%d/%d)", resource, tries, maxTries));
-            try (FileChannel channel = FileChannel.open(local, StandardOpenOption.WRITE, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING))
-            {
+            try (FileChannel channel = FileChannel.open(local, StandardOpenOption.WRITE, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)) {
                 URLConnection conn = resource.openConnection();
                 conn.setConnectTimeout(10000);
                 conn.setReadTimeout(30000);
@@ -169,15 +111,10 @@ public class Program
                 conn.setDoOutput(false);
                 channel.transferFrom(Channels.newChannel(conn.getInputStream()), 0L, Long.MAX_VALUE);
                 return Optional.of(local);
-            }
-            catch (IOException e)
-            {
+            } catch (IOException e) {
                 e.printStackTrace();
-            }
-            finally
-            {
-                if (Thread.interrupted())
-                {
+            } finally {
+                if (Thread.interrupted()) {
                     Thread.currentThread().interrupt();
                     break;
                 }
@@ -186,17 +123,19 @@ public class Program
         return Optional.empty();
     }
 
-    static Optional<URL> resource(String resource)
-    {
-        try
-        {
+    static Optional<URL> resource(String resource) {
+        try {
             return Optional.of(new URL(resource));
-        }
-        catch (MalformedURLException e)
-        {
+        } catch (MalformedURLException e) {
             e.printStackTrace();
             return Optional.empty();
         }
+    }
+
+    interface Processor {
+
+        void accept(BufferedReader in, BufferedWriter out) throws IOException;
+
     }
 
 }
