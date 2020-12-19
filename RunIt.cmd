680ec571470b03718aff9e2dd2befbb89daff0a9
Lain MultipleInstances <zlainsama@163.com>
2020-12-19 22:28:35 +0800

    update

--- RunIt.cmd
+++ RunIt.cmd
@@ -4,11 +4,9 @@ java -jar MyDnsTool.jar
 
 if %ERRORLEVEL%==0 (
   del /Q /F /S ..\unbound\unbound_ad_servers
-  del /Q /F /S ..\unbound\unbound_blocked_servers
   del /Q /F /S ..\unbound\unbound_localforwards
 
   copy AdServers_Processed ..\unbound\unbound_ad_servers
-  copy MalwareDomains_Processed ..\unbound\unbound_blocked_servers
   copy LocalForwards_Processed ..\unbound\unbound_localforwards
 
   ..\unbound\unbound-checkconf.exe ..\unbound\service.conf
